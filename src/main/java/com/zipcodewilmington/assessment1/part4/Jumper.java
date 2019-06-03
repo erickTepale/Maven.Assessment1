40ef017556d4c54ea6debf3e9cbc12069cd99dbc
Erick Tepale <e.tepale@gmail.com>
2019-06-03 13:41:46 -0400

    finished part 4

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -5,7 +5,11 @@ public class Jumper {
     /*
      * Complete the function below.
      */
+    //                  flag height, custom jump height
     public int jumps(int k, int j) {
-        return -1;
+        Integer maxJumpCustom = k / j;
+        Integer remainder = k % j;
+        
+        return (maxJumpCustom + remainder);
     }
 }
