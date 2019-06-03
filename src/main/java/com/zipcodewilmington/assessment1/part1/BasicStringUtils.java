816112a31d2c3514415f7c7fc8032d0a158ce76c
Erick Tepale <e.tepale@gmail.com>
2019-06-03 09:41:09 -0400

    Done Baic String Utils

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -9,7 +9,10 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+        String [] arr = str.split("");
+        arr[0] = arr[0].toUpperCase();
+
+        return String.join("", arr);
     }
 
     /**
@@ -17,7 +20,13 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        String [] arr = str.split("");
+        String [] rev = new String [arr.length];
+
+        for (int i = 0; i < arr.length ; i++) {
+            rev[i] = arr[arr.length - i - 1];
+        }
+        return String.join("", rev);
     }
 
     /**
@@ -25,7 +34,7 @@ public class BasicStringUtils {
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+        return camelCase(reverse(str));
     }
 
 
@@ -34,7 +43,11 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+        String [] arr = str.split("");
+        String [] trim = new String[arr.length - 2];
+        System.arraycopy(arr, 1, trim,0, arr.length - 2);
+
+        return String.join("", trim);
     }
 
     /**
@@ -42,6 +55,16 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+        char [] split = str.toCharArray();
+
+        for (int i = 0; i <split.length ; i++) {
+            if(Character.isLowerCase(split[i])){
+                split[i] = Character.toUpperCase(split[i]);
+            }
+            else if(Character.isUpperCase(split[i])){
+                split[i] = Character.toLowerCase(split[i]);
+            }
+        }
+        return String.valueOf(split);
     }
 }
