9a1f742cb2eeb67280c41c595435823590b2d4d9
Erick Tepale <e.tepale@gmail.com>
2019-06-03 12:44:55 -0400

    everything except petOwnerTest works

--- src/main/java/com/zipcodewilmington/assessment1/part3/Pet.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/Pet.java
@@ -4,17 +4,27 @@ package com.zipcodewilmington.assessment1.part3;
  * Created by leon on 2/16/18.
  */
 public abstract class Pet implements Animal {
+    private String name;
+    private Integer age;
+    private PetOwner owner;
+
     /**
      * nullary constructor
      * by default, pet has age of 0; name of "";
      */
     public Pet() {
+        name = "";
+        age = 0;
+        owner = null;
     }
 
     /**
      * @param name name of this pet
      */
     public Pet(String name) {
+        this.age = 0;
+        this.name = name;
+        owner = null;
     }
 
 
@@ -22,6 +32,9 @@ public abstract class Pet implements Animal {
      * @param age age of this pet
      */
     public Pet(int age) {
+        this.name = "";
+        this.age = age;
+        owner = null;
     }
 
     /**
@@ -29,20 +42,22 @@ public abstract class Pet implements Animal {
      * @param age age of this pet
      */
     public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     /**
      * @return name of this pet
      */
     public String getName() {
-        return null;
+        return name;
     }
 
     /**
      * @return age of this pet
      */
     public Integer getAge() {
-        return null;
+        return age;
     }
 
     /**
@@ -50,12 +65,22 @@ public abstract class Pet implements Animal {
      * ensure this instance of `Pet` is added to the owner's composite `pets` list
      */
     public void setOwner(PetOwner newPetOwner) {
+        owner = newPetOwner;
     }
 
     /**
      * @return PetOwner object whose composite `pets` collection contains this Pet instance
      */
     public PetOwner getOwner() {
-        return null;
+
+        return owner;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
@@ -1,26 +1,56 @@
 package com.zipcodewilmington.assessment1.part3;
 
+import java.util.ArrayList;
+
 /**
  * Created by leon on 2/16/18.
  */
 public class PetOwner {
+    private String name;
+    private Pet[] petsArr;
     /**
      * @param name name of the owner of the Pet
      * @param pets array of Pet object
      */
     public PetOwner(String name, Pet... pets) {
+        this.name = name;
+
+        petsArr = pets;
+        assignOwnerToPets();
+    }
+
+    private void assignOwnerToPets(){
+        if(petsArr != null ) {
+            for (Pet i : petsArr)
+                i.setOwner(this);
+        }
     }
 
     /**
      * @param pet pet to be added to the composite collection of Pets
      */
     public void addPet(Pet pet) {
+        if(petsArr != null) {
+            Pet[] newPet = new Pet[petsArr.length + 1];
+            newPet[petsArr.length] = pet;
+            petsArr = newPet;
+        }else{
+            petsArr = new Pet[1];
+            petsArr[0] = pet;
+        }
+        assignOwnerToPets();
     }
 
     /**
      * @param pet pet to be removed from the composite collection Pets
      */
     public void removePet(Pet pet) {
+//        ArrayList<Pet> fresh = new ArrayList<>();
+//        for (int i = 0; i <pets.length; i++) {
+//            if(!( pet.getName().equals(pets[i].getName())) )
+//                fresh.add(pets[i]);
+//        }
+//        fresh.toArray(pets);
 
     }
 
@@ -61,14 +91,14 @@ public class PetOwner {
      * @return the number of Pet objects stored in this class
      */
     public Integer getNumberOfPets() {
-        return null;
+        return petsArr.length;
     }
 
     /**
      * @return the name property of the Pet
      */
     public String getName() {
-        return null;
+        return name;
     }
 
     /**
